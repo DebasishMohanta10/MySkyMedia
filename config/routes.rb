daf7cfbfc638841d8d07fe304016a6d9bec3f8f5
DebasishMohanta <debasish2018rta@gmail.com>
2022-11-12 14:57:20 +0530

    adding post

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts
   get '/home',to: 'pages#home',as: "home"
+  root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
